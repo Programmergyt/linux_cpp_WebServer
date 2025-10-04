fc758196af75ad74c97ee81e82bfddabf7a7d8d4
Programmergyt <2403508140@qq.com>
2025-10-04 21:50:27 +0800

    CMake + C++11 without build and executable files

--- scripts/git_save.sh
+++ scripts/git_save.sh
@@ -1,4 +1,4 @@
 git checkout -b cmake-cpp11    # 新建并切换到 cmake-cpp11 分支
 git add .                         # 添加所有项目文件
-git commit -m "Initial version: CMake + C++11"
+git commit -m "CMake + C++11 without build and executable files"  # 提交更改并添加注释
 git push origin cmake-cpp11    # 推送到远程仓库
